e42d266ee96f89e0dce7a216e0f18425c6411e2e
jairo ramos <jaramosm@ufpso.edu.co>
2023-05-15 13:11:35 -0500

    vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,32 +1,52 @@
 <script setup>
-const incrementar (){
-  
+
+
+import { ref } from 'vue';
+const solicitudes = ref(0)
+const titulo = ref('generador de solicitudes')
+
+const incrementar = function () {
+  console.log("incrementar")
+  solicitudes.value++;
+}
+const decrementar = () => {
+  if (solicitudes.value > 0) {
+    console.log("decrementar")
+    solicitudes.value--;
+  }
+
 }
 </script>
 
 <template>
+ <div>
+  <h2>{{ titulo }}</h2>
+ </div>
+
   <div class="wrapper">
     <svg id="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
       stroke="currentColor" class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round"
         d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
     </svg>
-    <div id="globito">
-      10
+    <div id="globito" v-if="solicitudes > 0">
+      {{ solicitudes }}
     </div>
-    
+
   </div>
   <br>
 
-  <span class="button">
+  <span @click="decrementar" class="button">
     -
-  </span> 
-  
-  <span class="button">
+  </span>
+
+  <span @click="incrementar" class="button">
     +
   </span>
-  
-
+  <br>
+  <span v-show="solicitudes == 0">nadie te copea :/</span>
+  <br>
+  <input type="text" v-model="titulo">
 </template>
 
 <style scoped>
@@ -63,12 +83,14 @@ const incrementar (){
   border-radius: 20%;
   margin: 10px;
 }
-.button:hover{
+
+.button:hover {
   background-color: rgb(153, 14, 14);
   color: black;
   cursor: pointer;
 }
-.button:active{
+
+.button:active {
   background-color: darkmagenta;
   color: black;
   cursor: pointer;
